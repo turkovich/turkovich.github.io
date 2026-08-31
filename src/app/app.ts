215db820b66769b896333432ad1239cdf51d8a03
Nikita Turkovich <nikita.turkovich@outlook.com>
2026-08-31 12:39:19 +0300

    fix(ui): exports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,23 @@
 import { Component, ChangeDetectionStrategy, AfterViewInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Tag, Link } from "./components/export";
-import { Button } from "./components/button/button";
-import { Label } from "./components/label/label";
-import { Input } from "./components/input/input";
-import { Switch } from "./components/switch/switch";
-import { Checkbox } from "./components/checkbox/checkbox";
-import { Radiobuton } from "./components/radiobuton/radiobuton";
-import { Tabs } from "./components/tabs/tabs";
-import { Tab } from "./components/tabs/tab";
-import { FadeIn } from "./directives/fade-in";
+import {
+  Button,
+  Tag,
+  Link,
+  Radiobuton,
+  Tabs,
+  Tab,
+  Checkbox,
+  Label,
+  Input,
+  Switch,
+} from './components/export';
+import { FadeInDirective } from "./directives/export";
 import Lenis from 'lenis';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Tag, Button, Label, Input, Switch, Checkbox, Radiobuton, Tabs, Link, Tab, FadeIn],
+  imports: [RouterOutlet, Tag, Button, Label, Input, Switch, Checkbox, Radiobuton, Tabs, Link, Tab, FadeInDirective],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.scss'
