d8df7652ed9aa76c25b31446bc647728a4e300b1
Nikita Turkovich <nikita.turkovich@outlook.com>
2026-08-31 13:27:46 +0300

    fix(ui): wait for fonts loading

--- src/app/directives/fade-in.ts
+++ src/app/directives/fade-in.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Directive, ElementRef, inject } from '@angular/core';
+import { afterNextRender, Directive, DOCUMENT, ElementRef, inject } from '@angular/core';
 
 @Directive({
   selector: '[ntFadeIn]',
@@ -8,22 +8,39 @@ import { afterNextRender, Directive, ElementRef, inject } from '@angular/core';
 })
 export class FadeInDirective {
   private el = inject(ElementRef<HTMLElement>);
+  private document = inject(DOCUMENT);
 
   constructor() {
     afterNextRender(() => {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              entry.target.classList.add('fade-in_is_visible');
-              observer.unobserve(entry.target);
-            }
-          });
-        },
-        { threshold: 0.15}
-      );
-
-      observer.observe(this.el.nativeElement);
+      this.initObserver();
     });
+  }
+
+  private async initObserver() {
+    // Ждём, пока браузер загрузит все шрифты
+    if ('fonts' in this.document) {
+      await (this.document as any).fonts.ready;
+    }
+
+    // Даём браузеру один кадр на пересчёт layout
+    requestAnimationFrame(() => {
+      this.createObserver();
+    });
+  }
+
+  private createObserver() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in_is_visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.15 }
+    );
+
+    observer.observe(this.el.nativeElement);
   }
 }
